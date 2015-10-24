47b1669906a8c25292703a6ef127b86ed5f125a5
seplanna <seplanna@gmail.com>
2015-10-24 21:19:03 +0200

    add scripts wit plots

---
+++ scripts/utils.py
@@ -0,0 +1,90 @@
+import matplotlib
+from pylab import *
+
+def Get_precision(data_file, res_file):
+    res_distance = 0
+    res = [[], []]
+    dists = []
+    labels = []
+    last_dist = -1
+    sum_label = 0
+    with open(data_file) as data:
+        for line_n, line in enumerate(data):
+            if (line_n%10**6 == 0):
+                print(line_n)
+            line = line.strip().split()
+            dist = float(line[-2])
+            label = float(line[-1])
+            labels.append(label)
+            if last_dist == dist:
+                sum_label += label
+            else:
+
+                if (line_n > 0):
+                    dists.append(last_dist)
+                    res[0].append(line_n)
+                    res[1].append(float(sum_label)/(line_n))
+                    if (res[1][-1] > 0.55):
+                        res_distance = last_dist
+                sum_label += label
+            last_dist = dist
+    l = len(res[0])
+    axis([0, 200, 0.2, 0.5])
+    #plot([float(r)/res[0][-1] for r in res[0]], res[1])
+    plot(labels)
+    grid(True)
+
+    savefig(res_file)
+    return str(res_distance)
+
+def PlotPrecision(data_file, res_file):
+    res = []
+    with open(data_file) as data:
+        for line_n, line in enumerate(data):
+            if (line_n%10**6 == 0):
+                print(line_n)
+            line = line.strip().split()
+            res.append(float(line[0]))
+    axis([0,500,0.4,0.5])
+    plot (res)
+    grid(True)
+    savefig(res_file)
+
+def GetCTR(data_file, res_file):
+    l  = 10000
+    res_labels = [0. for i in range(l)]
+    res_n_users = [0. for i in range(l)]
+    with open(data_file) as data:
+        for line_n, line in enumerate(data):
+            if (line_n%10**6 == 0):
+                print(line_n)
+            line = line.strip().split()[1:]
+            line1 = [float(line[i])*(i+1) for i in range(len(line))]
+            line[1:] = [line1[i+1] - line1[i] for i in range(len(line1)-1)]
+            #print(line)
+            #print(line1)
+            #line = line1
+            if (len(line) > l):
+                line = line[:l]
+            last_label = 0.
+            for i in range(len(line)):
+                label  = float(line[i])
+                if (label > 0.5):
+                    #if (i == 3):
+                        #print(line, last_label, label)
+                    res_labels[i] += 1
+                last_label = label
+                res_n_users[i] += 1
+    #res_labels = [res_labels[i]/(res_n_users[i]+1e-10) for i in range(10)]
+    #res_labels = "\n".join(str(r) for r in res_labels)
+    res_labels = "\n".join(str(res_labels[i]/res_n_users[i]) for i in range(l))
+    return res_labels
+
+#with open("../../../data_stat/histogramms/dist_271", 'w') as dis:
+#    for i in range(100,101,1):
+#        print(i)
+#        directory = "../../../data_stat/"
+#        dis.write(GetCTR(directory + "histogramms/histogramm_" + str(i), directory + "hist_png/histogramm_"
+#                                + str(i) + ".png") + " " + str(i) + "\n")
+PlotPrecision("../../../data_stat/histogramms/dist_271", "../../../data_stat/hist_png/hist_100.png")
+
